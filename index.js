9582dd84cb0a989b8fca8d789efe726a9ece2a36
hamzawz <34839937+hamzawz@users.noreply.github.com>
2020-07-15 01:38:25 +0200

    update 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
-    response.end("Hello Hamza Nice to meet");
+    response.end("Hello Hamza");
 });
 
 const port = process.env.PORT || 1337;
